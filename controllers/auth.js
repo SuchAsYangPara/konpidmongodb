392fc3fbc8cdddbfcd5ee88c8f4c2b8a8e5caa44
Icegoo141 <isoonthipthanasup@gmail.com>
2024-03-04 16:34:08 +0700

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,7 +18,7 @@ exports.register = async (req, res, next) => {
 
     sendTokenResponse(user, 200, res);
   } catch (err) {
-    res.status(400).json({ success: false });
+    res.status(400).json({ success: false, message: 'Cannot register' });
     console.log(err.stack);
   }
 };
